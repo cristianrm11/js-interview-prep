7cef80eb00ad97cc6a289b6a0fa27ff42a9293cf
cretana <cristianrm11@gmail.com>
2023-02-15 01:34:37 -0600

    resolve the output format to csv, next step verify recursion

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,35 @@
 
 import jsonFile from './data/input.json';
 
-function parseJsonToCsv(data: object) {
-    console.log(data);
+function spreadObjectToString(header: any, row: any, headers: Set<string>, rows: any[]) {
+    Object.keys(row).map((prop: any) => {
+        console.log(`prop: ${prop}`)
+        console.log(`row[prop]: ${row[prop]}`)
+        headers.add(`${header}.${prop}`);
+        rows.push(`${row[prop]}`);
+    });
+}
+
+
+function parseJsonToCsv(data: object[]) {
+    let headers = new Set<string>;
+    let rows: any[] = [];
+    if (typeof data !== 'object') return null;
+
+    data.map((item: any) => {
+        Object.keys(item).map((key) => {
+            if (typeof item[key] !== 'object') {
+                headers.add(key);
+                rows.push(item[key]);
+            } else {
+                spreadObjectToString(key, item[key], headers, rows);
+            }
+        })
+    });
+
+    console.log(Array.from(headers).join(','));
+    console.log(rows.join(','));
+
 }
 
 parseJsonToCsv(jsonFile);
